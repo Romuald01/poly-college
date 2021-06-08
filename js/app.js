b5b2ef8c88ff6e2f7956d705da02c532c44cc631
Romuald01 <romualdadjimagbe2018@gmail.com>
2021-06-08 01:09:49 +0100

    almost done polycollegeweb

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+  // code that stop the overflow in y-axis when it is scrolled.
+  document.body.classList.add("no-scroll");
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+
